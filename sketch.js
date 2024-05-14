d67f70f0ab5c3cdbae9b51fd09cdcd2a2c999072
Luciano J <167217867+LucianoPECTec@users.noreply.github.com>
2024-05-14 14:37:53 -0300

    Update com comentários sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
+// Configuração inicial: define o tamanho do canvas
 function setup() {
-  createCanvas(600, 600);
-  background ("white"10
+  createCanvas(600, 600); // Cria um canvas de largura 600 e altura 600 pixels
+  background("white"); // Define a cor de fundo do canvas como branco
 }
 
+// Função de desenho contínuo
 function draw() {
-  stroke ("blue");
-  fill("yellow")
+  stroke("blue"); // Define a cor da borda para azul
+  fill("yellow"); // Define a cor de preenchimento para amarelo
 
-  if(mouseIsPressed){
-    rect (mouseX, mouseY, 20,35);
+  // Verifica se o botão do mouse está pressionado
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35); // Desenha um retângulo na posição do mouse com largura 20 e altura 35
+  }
 }
